7fc94c5839baf4685be87c5e7e6e81ab4f6382bc
RonanKer29 <kervella.ronan@hotmail.com>
2024-12-13 12:53:31 +0100

    marquee for companies

--- components/Companies.tsx
+++ components/Companies.tsx
@@ -2,17 +2,44 @@ import Image from "next/image";
 import fluid from "../public/featured/Fluid.svg";
 import hikkeno from "../public/featured/Hikkeno.svg";
 import bubble from "../public/featured/Bubble.svg";
+import Marquee from "react-fast-marquee";
 
 const Companies = () => {
   return (
-    <div className="items-center mx-auto">
-      <h1 className=" text-xl text-center font-dmsans text-dark/50 font-medium leading-[46px]">
-        We have the fast paced growing companies with us
+    <div className="relative flex flex-col items-center mx-auto max-w-[800px]">
+      <h1 className="text-xl text-center font-dm-sans text-dark/50 font-medium leading-[46px]">
+        We have the fast-paced growing companies with us
       </h1>
-      <div className="flex justify-between mx-auto mt-9 max-w-[1000px]">
-        <Image src={fluid} alt="fluid" />
-        <Image src={hikkeno} alt="hikkeno" />
-        <Image src={bubble} alt="bubble" />
+      <div className="relative w-full overflow-hidden mt-9">
+        {/* Left Blur */}
+        <div className="absolute top-0 left-0 z-10 w-12 h-full pointer-events-none bg-gradient-to-r from-white to-transparent" />
+
+        {/* Right Blur */}
+        <div className="absolute top-0 right-0 z-10 w-12 h-full pointer-events-none bg-gradient-to-l from-white to-transparent" />
+
+        <Marquee speed={50} pauseOnHover={true}>
+          <div className="flex items-center justify-center mx-10">
+            <Image
+              src={fluid}
+              alt="fluid"
+              className="w-[150px] h-auto object-contain"
+            />
+          </div>
+          <div className="flex items-center justify-center mx-10">
+            <Image
+              src={hikkeno}
+              alt="hikkeno"
+              className="w-[150px] h-auto object-contain"
+            />
+          </div>
+          <div className="flex items-center justify-center mx-10">
+            <Image
+              src={bubble}
+              alt="bubble"
+              className="w-[150px] h-auto object-contain"
+            />
+          </div>
+        </Marquee>
       </div>
     </div>
   );
