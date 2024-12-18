f134b821b62ccf4c09389ff4b70633de8f49137e
RonanKer29 <kervella.ronan@hotmail.com>
2024-12-18 13:13:02 +0100

    growth component

--- components/Growth.tsx
+++ components/Growth.tsx
@@ -11,25 +11,25 @@ const Growth = () => {
 
   return (
     <div className="flex flex-col items-center justify-center mt-36 space-y-6 text-center">
-      <h1 className="text-5xl  font-bold leading-tight text-dark font-dmsans w-[1200px]">
-        Turn your growth in to Squaree
+      <h1 className="text-5xl font-bold leading-tight text-dark font-dmsans w-[1200px]">
+        Turn your growth into Squaree
       </h1>
-      <h2 className="text-lg  text-darkGray w-[900px] text-center font-manrope font-medium leading-[1.65]">
+      <h2 className="text-lg text-darkGray w-[900px] text-center font-manrope font-medium leading-[1.65]">
         The expectation that productivity should always lead to tangible results
         or accomplishments & notion that certain types of work or activities are
         more valuable or productive than others.
       </h2>
       <Image src={graph} alt="graph" width={374} height={157} />
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-[1200px] justify-evenly">
+      <div className="flex w-[1200px] justify-between">
         {statistics.map((statistic, index) => (
           <div
             key={index}
-            className="flex flex-col items-center justify-between space-y-2 max-w-[200px] "
+            className="flex flex-col items-center space-y-2 max-w-[200px]"
           >
             <h1 className="text-6xl font-bold text-dark">
               {statistic.percentage}
             </h1>
-            <p className="text-darkGray font-dmsans text-base items-center justify-center">
+            <p className="text-darkGray font-dmsans text-base text-center">
               {statistic.description}
             </p>
           </div>
